94995e711e78e8c3cd78446a8b7722909373c9b9
annisatahira <annisatahira999@gmail.com>
2020-08-11 16:00:01 +0700

    basic redux

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/playground/redux-101.js',
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
-};
+    historyApiFallback: true,
+  },
+}
