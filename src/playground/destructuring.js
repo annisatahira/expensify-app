6941acbe873c092a1447aff9ab1e2718ca2e7c0e
annisatahira <annisatahira999@gmail.com>
2020-08-12 10:36:46 +0700

    refactoring and organizing action generators

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -10,22 +10,22 @@
 // }
 
 // //destructuring object
-// const { name: firstName = 'Anonymous', age } = Person
-// const { city, temp } = Person.location
+const { name: firstName = 'Anonymous', age } = Person
+const { city, temp } = Person.location
 
-// console.log(`Hello ${firstName}`)
-// console.log(`He is from ${city}`)
+console.log(`Hello ${firstName}`)
+console.log(`He is from ${city}`)
 
-// const book = {
-//   title: 'Ego is Enemy',
-//   author: 'Ryan Holiday',
-//   publisher: {
-//     // name: 'Penguin',
-//   },
-// }
+const book = {
+  title: 'Ego is Enemy',
+  author: 'Ryan Holiday',
+  publisher: {
+    // name: 'Penguin',
+  },
+}
 
-// const { title } = book
-// const { name: publisherName = 'Self Publisher' } = book.publisher
+const { title } = book
+const { name: publisherName = 'Self Publisher' } = book.publisher
 
 // console.log(`${title} is a good book from publiser ${publisherName}`)
 
--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,18 +1,43 @@
-import { createStore, bindActionCreators } from 'redux'
+import { createStore } from 'redux'
+
+//Action generators - functions that return action object
+
+//example
+
+// const add = ({ a, b }, c) => {
+//   return a + b + c
+// }
+
+// console.log(add({ a: 1, b: 12 }, 100))
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy,
+})
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy,
+})
+
+const reset = () => ({
+  type: 'RESET',
+})
+
+const set = ({ count }) => ({
+  type: 'SET',
+  count,
+})
 
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy =
-        typeof action.incrementBy === 'number' ? action.incrementBy : 1
       return {
-        count: state.count + incrementBy,
+        count: state.count + action.incrementBy,
       }
     case 'DECREMENT':
-      const decrementBy =
-        typeof action.decrementBy === 'number' ? action.decrementBy : 1
       return {
-        count: state.count - decrementBy,
+        count: state.count - action.decrementBy,
       }
     case 'SET':
       return {
@@ -31,29 +56,19 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState())
 })
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5,
-})
+store.dispatch(incrementCount({ incrementBy: 5 }))
+//apabila menggunakan cara ini typo tiidak akan terdeteksi
+// store.dispatch({
+//   type: 'INCREMENT',
+// })
 
-store.dispatch({
-  type: 'INCREMENT',
-})
+//menggunakan function
+store.dispatch(incrementCount())
 
-store.dispatch({
-  type: 'RESET',
-})
+store.dispatch(reset())
 
-store.dispatch({
-  type: 'DECREMENT',
-})
+store.dispatch(decrementCount())
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10,
-})
+store.dispatch(decrementCount({ decrementBy: 10 }))
 
-store.dispatch({
-  type: 'SET',
-  count: 101,
-})
+store.dispatch(set({ count: -101 }))
