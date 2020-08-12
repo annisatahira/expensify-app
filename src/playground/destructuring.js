209cc3c1cf1fabc61953c4b01e29e437ebad2eea
annisatahira <annisatahira999@gmail.com>
2020-08-12 09:32:13 +0700

    array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,5 @@
+//Object Destructuring
+
 // const Person = {
 //   name: 'Andrew',
 //   age: 27,
@@ -14,15 +16,31 @@
 // console.log(`Hello ${firstName}`)
 // console.log(`He is from ${city}`)
 
-const book = {
-  title: 'Ego is Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    // name: 'Penguin',
-  },
-}
+// const book = {
+//   title: 'Ego is Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     // name: 'Penguin',
+//   },
+// }
 
-const { title } = book
-const { name: publisherName = 'Self Publisher' } = book.publisher
+// const { title } = book
+// const { name: publisherName = 'Self Publisher' } = book.publisher
 
-console.log(`${title} is a good book from publiser ${publisherName}`)
+// console.log(`${title} is a good book from publiser ${publisherName}`)
+
+//Array Destructuring
+
+const address = ['25 Lembur Gedong Street', 'Sumedang', 'Indonesia', '116112']
+
+const [street, city, nation, zip] = address
+
+console.log(
+  `i've used to living in ${street} ${city} city with zip code ${zip}`,
+)
+
+const item = ['Coffe (hot)', 'Rp.20k', 'Rp.50k', 'Rp.55k']
+
+const [coffe, largeSizePrice] = item
+
+console.log(`Harga ${coffe} ukuran large harganya ${largeSizePrice}`)
